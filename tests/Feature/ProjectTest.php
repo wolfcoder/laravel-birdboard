ebc696de0fff806b846d9eef496f0e04c829a589
Bams <bbg2021@gmail.com>
2024-11-24 10:20:46 +0800

    BIB-3 #time 1h15m  #comment Calling factory is different in the tinker

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Project;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,7 +11,7 @@ class ProjectTest extends TestCase
 {
 
     // RefreshDatabase
-    use WithFaker;
+    use WithFaker, RefreshDatabase;
 
     public function test_user_can_create_a_project(): void
     {
@@ -26,9 +27,25 @@ class ProjectTest extends TestCase
         $this->post('/projects', $attributes)->assertRedirect('/projects');
 
         // check if the data is in the database
-        // $this->assertDatabaseHas('projects', $attributes);
+        $this->assertDatabaseHas('projects', $attributes);
 
         // // get the data from the database
-        // $this->get('/projects')->assertSee($attributes['title']);
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+    // php artisan test --filter test_a_project_requires_a_title
+    public function test_a_project_requires_a_title()
+    {
+        // factory attribute
+        // $attributes = Project::factory()->make(['title' => ''])->toArray();
+        $attributes = Project::factory()->raw(['title' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    // php artisan test --filter test_a_project_requires_a_description
+    public function test_a_project_requires_a_description()
+    {
+        $attributes = Project::factory()->raw(['description' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
 }
